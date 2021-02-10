f3f5ebe4ef4ebc08e3134d8e3abad81431344ae0
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-02-10 11:20:07 -0600

    Finsh countdown logic

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -7,18 +7,59 @@ const Countdown = () => {
   const [countdownMinutes, setCountdownMinutes] = useState("00");
   const [countdownSeconds, setCountdownSeconds] = useState("00");
 
+  let timeInterval = useRef();
+
+  const startCountdown = () => {
+    const COUNTDOWN_START_DATE = new Date("March 26, 2021 00:00:00").getTime();
+    timeInterval = setInterval(() => {
+      const NOW = new Date().getTime();
+      const distance = (COUNTDOWN_START_DATE = NOW);
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (100 * 60 * 60)
+      );
+      //   const HOURS = ("0" + hours).slice(-2);
+
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setCountdownDays(days);
+        setCountdownHours(hours);
+        setCountdownMinutes(minutes);
+        setCountdownSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startCountdown();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
+
   return (
     <div className="Countdown">
       <div className="info__countdown--time">
+        <p className="zeros">{timerDays}</p>
         <p>Days</p>
       </div>
       <div className="info__countdown--time">
+        <p className="zeros">{timerHours}</p>
         <p>Hours</p>
       </div>
       <div className="info__countdown--time">
+        <p className="zeros">{timerMinutes}</p>
         <p>Minutes</p>
       </div>
       <div className="info__countdown--time">
+        <p className="zeros">{timerSeconds}</p>
         <p>Seconds</p>
       </div>
     </div>
