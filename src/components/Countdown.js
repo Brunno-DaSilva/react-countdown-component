085e5beb5a7d5034ed30dfbfcc8008028927755f
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-02-10 11:09:25 -0600

    Added backgruond and font

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./countdown.scss";
 
 const Countdown = () => {
+  const [countdownDays, setCountdownDays] = useState("00");
+  const [countdownHours, setCountdownHours] = useState("00");
+  const [countdownMinutes, setCountdownMinutes] = useState("00");
+  const [countdownSeconds, setCountdownSeconds] = useState("00");
+
   return (
     <div className="Countdown">
       <div></div>
